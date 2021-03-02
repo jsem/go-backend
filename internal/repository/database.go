22a28aeee162a17c94b3668efceae4e983c89211
jsem <james.robert.semple@gmail.com>
2021-03-02 17:41:26 +1100

    Initial Commit

---
+++ internal/repository/database.go
@@ -0,0 +1,12 @@
+package repository
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDatabaseConnection() (db *gorm.DB, err error) {
+	dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return
+}
